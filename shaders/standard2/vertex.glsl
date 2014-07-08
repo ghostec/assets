2ed476c11d134a5afd32bca1bcee609201202431
Yuri Brito <yuriabrito@gmail.com>
2014-07-08 02:19:22 -0300

    MousePicking working again (after new draw flow)

--- shaders/standard2/vertex.glsl
+++ shaders/standard2/vertex.glsl
@@ -4,6 +4,10 @@ in vec3 vPosition;
 in vec3 vUV;
 in vec3 vNormal;
 
+out vec2 fragTexCoord;
+out vec3 fragVert;
+out vec3 fragNormal;
+
 uniform mat4 Model;
 
 uniform struct Camera
@@ -15,5 +19,8 @@ uniform struct Camera
 void main() 
 {
   // Pass some variables to the fragment shader
+  fragTexCoord = vec2( vUV.x, vUV.y );
+  fragNormal = vNormal;
+  fragVert = vPosition; 
   gl_Position = camera.proj * camera.view * Model * vec4(vPosition,1.0);
 }
